f433f00cf00db3bb12d11d6ef47328d866670195
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-11 18:10:47 -0400

    Add basic setup file for packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='duration-generator',
+      version='0.1',
+      description='A library of generators that allow looping for durations rather than numbers of iterations',
+      author='Ramsey Barghouti',
+      license='GPL3',
+      packages=['duration'],
+      zip_safe=False)
